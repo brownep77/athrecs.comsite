4a771c56e530674c5e773bd7456f7e080c5ec744
brownep77 <paultwbrowne@gmail.com>
2026-08-10 18:51:33 +0100

    Run Norwich 2025 batch 1: results places 1-100 module + wire into results.ts

--- src/data/results.ts
+++ src/data/results.ts
@@ -1,5 +1,6 @@
 import type { ResultSeed } from "./types";
 import { resultsA } from "./results-a";
 import { resultsB } from "./results-b";
+import { resultsRn2025B1 } from "./results-rn2025-b1";
 
-export const results: ResultSeed[] = [...resultsA, ...resultsB];
+export const results: ResultSeed[] = [...resultsA, ...resultsB, ...resultsRn2025B1];
