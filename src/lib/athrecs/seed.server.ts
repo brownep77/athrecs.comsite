2766003994d52278e958df8dc9562825d9af3fb9
brownep77 <paultwbrowne@gmail.com>
2026-08-17 23:42:18 +0100

    Advance Neon seed for UK half-marathon batch seven

--- src/lib/athrecs/seed.server.ts
+++ src/lib/athrecs/seed.server.ts
@@ -12,7 +12,7 @@ import {
 import { editionReplacements } from "@/data/entry-options";
 import { ensureAthleticsTaxonomy } from "./athletics-taxonomy.server";
 
-const SEED_VERSION = "athrecs-uk-half-marathon-entry-batch-six-v92";
+const SEED_VERSION = "athrecs-uk-half-marathon-entry-batch-seven-v93";
 const EXPECTED = catalogueMetadata.merged_counts;
 
 type Sql = Awaited<ReturnType<typeof getSql>>;
