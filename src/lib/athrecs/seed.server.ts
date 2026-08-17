ba92f8996e29d610cebb648469e3670fd22da563
brownep77 <paultwbrowne@gmail.com>
2026-08-17 22:47:33 +0100

    Advance half marathon entry seed

--- src/lib/athrecs/seed.server.ts
+++ src/lib/athrecs/seed.server.ts
@@ -12,7 +12,7 @@ import {
 import { editionReplacements } from "@/data/entry-options";
 import { ensureAthleticsTaxonomy } from "./athletics-taxonomy.server";
 
-const SEED_VERSION = "athrecs-uk-half-marathon-entry-batch-one-v86";
+const SEED_VERSION = "athrecs-uk-half-marathon-entry-batch-two-v87";
 const EXPECTED = catalogueMetadata.merged_counts;
 
 type Sql = Awaited<ReturnType<typeof getSql>>;
