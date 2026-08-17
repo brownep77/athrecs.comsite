ee5601a126dc2a20e705b9d92d106092873ccb19
brownep77 <paultwbrowne@gmail.com>
2026-08-18 00:37:58 +0100

    Advance catalogue seed to v99

--- src/lib/athrecs/seed.server.ts
+++ src/lib/athrecs/seed.server.ts
@@ -12,7 +12,7 @@ import {
 import { editionReplacements, eventSlugAliases } from "@/data/entry-options";
 import { ensureAthleticsTaxonomy } from "./athletics-taxonomy.server";
 
-const SEED_VERSION = "athrecs-uk-half-marathon-entry-batch-ten-hotfix-v98";
+const SEED_VERSION = "athrecs-uk-half-marathon-entry-batch-eleven-v99";
 const EXPECTED = catalogueMetadata.merged_counts;
 
 type Sql = Awaited<ReturnType<typeof getSql>>;
