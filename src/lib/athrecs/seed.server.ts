cb318f0de83e398149d720a80a04224e360a0099
brownep77 <paultwbrowne@gmail.com>
2026-08-18 23:05:13 +0100

    Advance UK 10K seed marker to v171

--- src/lib/athrecs/seed.server.ts
+++ src/lib/athrecs/seed.server.ts
@@ -12,7 +12,7 @@ import {
 import { editionReplacements, eventSlugAliases } from "@/data/entry-options";
 import { ensureAthleticsTaxonomy } from "./athletics-taxonomy.server";
 
-const SEED_VERSION = "athrecs-uk-10k-entry-batch-twenty-five-v170";
+const SEED_VERSION = "athrecs-uk-10k-entry-batch-twenty-six-v171";
 const EXPECTED = catalogueMetadata.merged_counts;
 
 type Sql = Awaited<ReturnType<typeof getSql>>;
