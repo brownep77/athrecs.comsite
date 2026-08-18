2b3ca2fb29964ff317b7ef0b440fdafcb791ffae
brownep77 <paultwbrowne@gmail.com>
2026-08-18 20:53:38 +0100

    Advance UK 10K seed to batch 17

--- src/lib/athrecs/seed.server.ts
+++ src/lib/athrecs/seed.server.ts
@@ -12,7 +12,7 @@ import {
 import { editionReplacements, eventSlugAliases } from "@/data/entry-options";
 import { ensureAthleticsTaxonomy } from "./athletics-taxonomy.server";
 
-const SEED_VERSION = "athrecs-uk-10k-entry-batch-sixteen-hotfix-v160";
+const SEED_VERSION = "athrecs-uk-10k-entry-batch-seventeen-v161";
 const EXPECTED = catalogueMetadata.merged_counts;
 
 type Sql = Awaited<ReturnType<typeof getSql>>;
