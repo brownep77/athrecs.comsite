119b347c1fbbdc3a153b1bfecd83712351124b06
brownep77 <paultwbrowne@gmail.com>
2026-08-18 05:06:39 +0100

    Advance persistent catalogue seed to v125

--- src/lib/athrecs/seed.server.ts
+++ src/lib/athrecs/seed.server.ts
@@ -12,7 +12,7 @@ import {
 import { editionReplacements, eventSlugAliases } from "@/data/entry-options";
 import { ensureAthleticsTaxonomy } from "./athletics-taxonomy.server";
 
-const SEED_VERSION = "athrecs-uk-half-marathon-entry-batch-thirty-five-v124";
+const SEED_VERSION = "athrecs-uk-half-marathon-entry-batch-thirty-six-v125";
 const EXPECTED = catalogueMetadata.merged_counts;
 
 type Sql = Awaited<ReturnType<typeof getSql>>;
