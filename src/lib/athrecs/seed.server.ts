e1abebe7ea09843c8ca330c1cd7092a1b2d9a64f
brownep77 <paultwbrowne@gmail.com>
2026-08-19 01:56:39 +0100

    Advance race catalogue seed to v184

--- src/lib/athrecs/seed.server.ts
+++ src/lib/athrecs/seed.server.ts
@@ -12,7 +12,7 @@ import {
 import { editionReplacements, eventSlugAliases } from "@/data/entry-options";
 import { ensureAthleticsTaxonomy } from "./athletics-taxonomy.server";
 
-const SEED_VERSION = "athrecs-uk-10k-entry-batch-thirty-eight-v183";
+const SEED_VERSION = "athrecs-uk-10k-entry-batch-thirty-nine-v184";
 const EXPECTED = catalogueMetadata.merged_counts;
 
 type Sql = Awaited<ReturnType<typeof getSql>>;
