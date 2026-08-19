8e57706356b405364fc22cc144e1335ffbf0d639
brownep77 <paultwbrowne@gmail.com>
2026-08-19 05:52:59 +0100

    Advance catalogue seed for stage 57

--- src/lib/athrecs/seed.server.ts
+++ src/lib/athrecs/seed.server.ts
@@ -12,7 +12,7 @@ import {
 import { editionReplacements, eventSlugAliases } from "@/data/entry-options";
 import { ensureAthleticsTaxonomy } from "./athletics-taxonomy.server";
 
-const SEED_VERSION = "athrecs-uk-10k-entry-batch-fifty-six-v201";
+const SEED_VERSION = "athrecs-uk-10k-entry-batch-fifty-seven-v202";
 const EXPECTED = catalogueMetadata.merged_counts;
 
 type Sql = Awaited<ReturnType<typeof getSql>>;
