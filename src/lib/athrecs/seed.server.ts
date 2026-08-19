2193ee93855df367575c3221e6866c52a53b6ec2
brownep77 <paultwbrowne@gmail.com>
2026-08-19 04:40:57 +0100

    Stage 51: update src/lib/athrecs/seed.server.ts

--- src/lib/athrecs/seed.server.ts
+++ src/lib/athrecs/seed.server.ts
@@ -12,7 +12,7 @@ import {
 import { editionReplacements, eventSlugAliases } from "@/data/entry-options";
 import { ensureAthleticsTaxonomy } from "./athletics-taxonomy.server";
 
-const SEED_VERSION = "athrecs-uk-10k-entry-batch-fifty-v195";
+const SEED_VERSION = "athrecs-uk-10k-entry-batch-fifty-one-v196";
 const EXPECTED = catalogueMetadata.merged_counts;
 
 type Sql = Awaited<ReturnType<typeof getSql>>;
