437c58c40bf48f7c9233f50cfd3e6cc70a7e65c7
brownep77 <paultwbrowne@gmail.com>
2026-08-28 08:05:28 +0100

    Add Belgium elite and youth competition rows 5

---
+++ src/data/belgium-elite-youth-competition-rows-5.ts
@@ -0,0 +1,23 @@
+/** Compact restricted-entry fixture rows, part 5 of 5. */
+export const BELGIUM_ELITE_YOUTH_ROWS_5 = `
+be-youth-cx-haacht	Haacht Youth Cyclo-cross	C	m	2	Haacht		2	e	m	m	Y	Y	w	2026-11-01	O				11
+be-youth-cx-denderhoutem	Denderhoutem Youth Cyclo-cross	C	m	5	Denderhoutem		2	e	m	m	Y	Y	v	2026-11-07	O				11
+be-youth-cx-jabbeke	Jabbeke Youth Cyclo-cross	C	m	1	Jabbeke		2	e	m	m	Y	Y	w	2026-11-08	O				11
+be-youth-cx-maldegem-november	Maldegem Youth Cyclo-cross	C	m	5	Maldegem		2	e	m	m	Y	Y	v	2026-11-11	O				11
+be-youth-cx-houthalen-helchteren	Houthalen-Helchteren Youth Cyclo-cross	C	m	8	Houthalen-Helchteren		2	e	m	m	Y	Y	v	2026-11-14	O				11
+be-youth-cx-ravestein-parkcross-hever	Ravestein Parkcross Hever Youth & G-Cyclocross	C	m	2	Hever	Ravestein Park, Hever	2	e	m	m	Y	Y	x	2026-11-22	O				11
+be-youth-cx-meulebeke-november	Meulebeke Youth Cyclo-cross	C	m	1	Meulebeke		2	e	m	m	Y	Y	w	2026-11-29	O				11
+be-youth-cx-pelt	Pelt Youth Cyclo-cross	C	m	8	Pelt		2	e	m	m	Y	Y	v	2026-12-20	O				11
+be-youth-cx-genk	Genk Youth & G-Cyclocross	C	m	8	Genk		2	e	m	m	Y	Y	t	2026-12-26	O				11
+be-youth-cx-huldenberg	Huldenberg Youth Cyclo-cross	C	m	2	Huldenberg		2	e	m	m	Y	Y	s	2026-12-27	O				11
+be-youth-cx-hamme-december	Hamme Youth Cyclo-cross	C	m	5	Hamme		2	e	m	m	Y	Y	v	2026-12-30	O				11
+be-youth-cx-bekkevoort	Bekkevoort Youth & G-Cyclocross	C	m	2	Bekkevoort		2	e	m	m	Y	Y	t	2027-01-16	O				11
+be-youth-cx-meer-hoogstraten	Meer-Hoogstraten Youth & G-Cyclocross	C	m	4	Meer	Meer, Hoogstraten	2	e	m	m	Y	Y	t	2027-01-23	O				11
+be-youth-cx-hoenderdaal-hoeilaart	Jan van Ruusbroeckpark Hoeilaart Youth Cyclo-cross	C	m	2	Hoeilaart	Jan van Ruusbroeckpark, Hoeilaart	2	e	m	m	Y	Y	v	2027-02-06	O				11
+be-youth-cx-leopoldsburg	Leopoldsburg Youth Cyclo-cross	C	m	8	Leopoldsburg		2	e	m	m	Y	Y	y	2027-02-13	O				11
+be-youth-road-topcompetition-jemeppe-sur-meuse	Topcompetition U17 Jemeppe-sur-Meuse	C	n	0	Jemeppe-sur-Meuse		0	e	o	o	Y	Y	z	2026-09-27	O				12
+be-youth-road-topcompetition-affligem	Topcompetition U17 Affligem	C	n	2	Affligem		0	e	o	o	Y	Y	z	2026-10-11	O				13
+be-junior-track-world-championships-heusden-zolder-2026	2026 UCI Junior Track World Championships — Heusden-Zolder	C	o	8	Heusden-Zolder		3	f	p	p	Y	F	c	2026-08-19	F			2026-08-23	14
+be-uec-bmx-racing-championships-heusden-zolder-2027	2027 UEC BMX Racing European Championships — Heusden-Zolder	C	p	8	Heusden-Zolder		4	g	p	p	M	F	10	2027-07-09	C			2027-07-11	15
+be-uec-track-junior-u23-championships-heusden-zolder-2027	2027 UEC Track Junior & U23 European Championships — Heusden-Zolder	C	q	8	Heusden-Zolder		3	g	p	p	M	F	11	2027-07-13	C			2027-07-18	14
+`;
