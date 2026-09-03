00d0378581aa5cbb2c274023e2e571139385d916
brownep77 <paultwbrowne@gmail.com>
2026-09-03 23:48:37 +0100

    Add six more non-duplicate races and publish
    Sioux Falls, Fox Cities, Lincoln, Med City, Fargo, EPIC Green Bay.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7441,
-    editions: 219154,
+    race_series: 7447,
+    editions: 219160,
     results: 2506,
   },
   clubs_source:
