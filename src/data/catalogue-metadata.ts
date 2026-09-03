1996ba8d5384a67132c39d42c57464e37b3b3e82
brownep77 <paultwbrowne@gmail.com>
2026-09-03 23:47:15 +0100

    Add six more non-duplicate races and publish
    Omaha, Duke City, Route 66 Tulsa, Las Vegas, Mesa, El Paso.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7435,
-    editions: 219148,
+    race_series: 7441,
+    editions: 219154,
     results: 2506,
   },
   clubs_source:
