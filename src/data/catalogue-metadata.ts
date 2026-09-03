6f8bb6e347264faed3b0a1b7e4a5435b4a1f485c
brownep77 <paultwbrowne@gmail.com>
2026-09-04 00:02:13 +0100

    Add six more non-duplicate races and publish
    Melbourne, Cadbury, Hobart, Canberra, Great Ocean Road, City2Surf.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7474,
-    editions: 219189,
+    race_series: 7480,
+    editions: 219195,
     results: 2506,
   },
   clubs_source:
