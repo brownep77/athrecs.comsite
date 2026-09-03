2c58fc8b77bd479b7449d5da157b669efda0ff43
brownep77 <paultwbrowne@gmail.com>
2026-09-04 00:39:09 +0100

    Add Dead Sea races and Spain starters
    Israel Dead Sea Marathon, Jordan Dead Sea Ultra, Malaga and San Sebastian.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7534,
-    editions: 219249,
+    race_series: 7538,
+    editions: 219253,
     results: 2506,
   },
   clubs_source:
