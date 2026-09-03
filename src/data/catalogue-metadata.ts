dc2f094e867f41d66353cb692d8f520dc601f5dd
brownep77 <paultwbrowne@gmail.com>
2026-09-03 23:08:01 +0100

    Add seven more non-duplicate races and publish
    Gyeongju, JTBC Seoul, Daegu, Seoul, Eugene, Vancouver, Ottawa.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7369,
-    editions: 219082,
+    race_series: 7376,
+    editions: 219089,
     results: 2506,
   },
   clubs_source:
