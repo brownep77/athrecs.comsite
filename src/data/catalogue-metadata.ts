f2e0fa1626c3683606bb0608dcbf8c13466568a1
brownep77 <paultwbrowne@gmail.com>
2026-09-04 00:17:15 +0100

    Add four more non-duplicate races and publish
    Great North Run, Cardiff Half, Belfast City Marathon, Leeds Half.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7498,
-    editions: 219213,
+    race_series: 7502,
+    editions: 219217,
     results: 2506,
   },
   clubs_source:
