3277f958f07ebdeec40de611b5a56dea716d1814
brownep77 <paultwbrowne@gmail.com>
2026-09-03 23:13:36 +0100

    Add six more non-duplicate races and publish
    Guayaquil, Cali, Bay to Breakers, BOLDERBoulder, Peachtree, Falmouth.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7389,
-    editions: 219102,
+    race_series: 7395,
+    editions: 219108,
     results: 2506,
   },
   clubs_source:
