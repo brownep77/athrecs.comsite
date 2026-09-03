88e44f9c7a04a6b17c8c9a3a728a7f8c46ca2701
brownep77 <brownep77@users.noreply.github.com>
2026-09-03 22:53:46 +0000

    Add six Southeast US races and republish
    Savannah Southern Half, Gulf Coast, Mobile, Charleston Half,
    Myrtle Beach and Knoxville.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7453,
-    editions: 219166,
+    race_series: 7459,
+    editions: 219172,
     results: 2506,
   },
   clubs_source:
