aca8032f3c00da6e2c253523bd60fb4734d3d21a
brownep77 <paultwbrowne@gmail.com>
2026-09-04 00:24:40 +0100

    Add nine more non-duplicate races and publish
    Manchester, Bournemouth, Snowdonia Eryri, Great Bristol Run, Stirling Half, Rotterdam, Copenhagen Half, two Helsinki marathons.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7506,
-    editions: 219221,
+    race_series: 7515,
+    editions: 219230,
     results: 2506,
   },
   clubs_source:
