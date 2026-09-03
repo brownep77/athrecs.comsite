3a952ae168bd23ae9cab3ab2dd38f08ce6c86395
brownep77 <paultwbrowne@gmail.com>
2026-09-04 00:30:26 +0100

    Add six more non-duplicate races and publish
    Trondheim, Malmo, Athens Authentic, Great Scottish Run, London 10,000, Cape Town.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7523,
-    editions: 219238,
+    race_series: 7529,
+    editions: 219244,
     results: 2506,
   },
   clubs_source:
