21fc73b4f3d626b33c514094b8ae37bd2252ff18
brownep77 <paultwbrowne@gmail.com>
2026-09-04 02:28:35 +0100

    Add 6 more September 2026 UK and NYC races
    Caernarfon, Glencoe, Reigate, Snowdonia Sea2Sky, Lakeland Maze Farm and NYC Endless Summer.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7828,
-    editions: 219543,
+    race_series: 7834,
+    editions: 219549,
     results: 2506,
   },
   clubs_source:
