51398475f8854e28fa25e73331187e76ef295a01
brownep77 <paultwbrowne@gmail.com>
2026-09-04 02:06:10 +0100

    Add 4 more September 2026 races Denmark Colombia Kenya Sweden
    Orestad, Bogota Women's Race, Enchilai and Lidingoloppet. No duplicate slugs.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7819,
-    editions: 219534,
+    race_series: 7823,
+    editions: 219538,
     results: 2506,
   },
   clubs_source:
