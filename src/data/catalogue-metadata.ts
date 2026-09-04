4dcf58726d54445b58d238e84eb8b9afb3235d13
brownep77 <paultwbrowne@gmail.com>
2026-09-04 02:21:02 +0000

    Fix October batch series and edition counts

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7901,
-    editions: 219616,
+    race_series: 7914,
+    editions: 219629,
     results: 2506,
   },
   clubs_source:
