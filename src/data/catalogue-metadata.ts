1ec36640b6e2ed52262b20270d0680ef923ddb0e
brownep77 <paultwbrowne@gmail.com>
2026-09-04 01:05:36 +0100

    Add five Southeast and Amiens races
    Big Beach, Pensacola Beach, Greenville, Rocket City, Amiens.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7572,
-    editions: 219287,
+    race_series: 7577,
+    editions: 219292,
     results: 2506,
   },
   clubs_source:
