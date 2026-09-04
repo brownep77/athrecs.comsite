6f0ac5c653d4a02a4adc031772ea844cea1c01d5
brownep77 <paultwbrowne@gmail.com>
2026-09-04 10:56:36 +0000

    Add 4 Angola Sep-Dec 2026 road races
    Petromar Ambriz, Lobito Run, Africell Color Run and Sao Silvestre Luanda.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7998,
-    editions: 219714,
+    race_series: 8002,
+    editions: 219718,
     results: 2506,
   },
   clubs_source:
