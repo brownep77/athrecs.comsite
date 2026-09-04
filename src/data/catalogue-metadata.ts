37721f3ed5fcfda2985674bfd281d1085bee842e
brownep77 <paultwbrowne@gmail.com>
2026-09-04 01:49:43 +0100

    Add 10 more September 2026 trail and South Africa races
    California trail halves plus SA and US city leftovers. No duplicate slugs.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7767,
-    editions: 219482,
+    race_series: 7777,
+    editions: 219492,
     results: 2506,
   },
   clubs_source:
