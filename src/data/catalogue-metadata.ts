2c4308bfa73dd3afce9cd4be4638e4c642793398
brownep77 <paultwbrowne@gmail.com>
2026-09-04 01:15:32 +0100

    Add 16 more AIMS 2027 races missing from RunRecs
    Second bulk slug-filter of dated AIMS 2027 events.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7603,
-    editions: 219318,
+    race_series: 7619,
+    editions: 219334,
     results: 2506,
   },
   clubs_source:
