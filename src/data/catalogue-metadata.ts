9cd2dccd5f7fbc92f286a0a317641bcab167d8f6
brownep77 <paultwbrowne@gmail.com>
2026-09-04 01:31:50 +0100

    Add 16 US fall city marathons missing from RunRecs
    USA Marathon List Oct-Nov 2026 dump. No duplicate slugs.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7676,
-    editions: 219391,
+    race_series: 7692,
+    editions: 219407,
     results: 2506,
   },
   clubs_source:
