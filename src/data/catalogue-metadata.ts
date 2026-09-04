361e98374b2aeed1ce01b6cd7e7658dd084e372b
brownep77 <paultwbrowne@gmail.com>
2026-09-04 01:07:53 +0100

    Add 10 AIMS calendar races missing from RunRecs
    Bulk slug-filter of AIMS Sep-Dec 2026 calendar.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7577,
-    editions: 219292,
+    race_series: 7587,
+    editions: 219302,
     results: 2506,
   },
   clubs_source:
