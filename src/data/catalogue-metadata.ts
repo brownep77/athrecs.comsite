069386b582e2806f7d257295e05c36dfaf0632d8
brownep77 <paultwbrowne@gmail.com>
2026-09-04 02:57:34 +0000

    Add 4 October 2026 races from New Zealand, Thailand and Vietnam
    Skipped Ha Long Bay Heritage Marathon (22 November 2026),
    VPBank Hanoi, Wakkanai, Auckland Marathon and Melbourne.

--- src/data/catalogue-metadata.ts
+++ src/data/catalogue-metadata.ts
@@ -13,8 +13,8 @@ export const catalogueMetadata = {
   merged_counts: {
     clubs: 1737,
     athletes: 1255,
-    race_series: 7963,
-    editions: 219678,
+    race_series: 7967,
+    editions: 219682,
     results: 2506,
   },
   clubs_source:
